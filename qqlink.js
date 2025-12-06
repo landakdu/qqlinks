304e2e6a0c3ac2a5cfa803a6aec9d2e50c76003d
landakdu <durilandak429@gmail.com>
2025-12-07 04:19:50 +0800

    Add files via upload

---
+++ qqlink.js
@@ -0,0 +1,72 @@
+document.getElementById('yourform').submit();
+function linknama() {location.replace("/");}
+function jumpnama() {window.open("/");}
+function linkqq288() {location.replace("https://qq288qsc.com/");}
+function jumpqq288() {location.replace("https://qq288qsc.com/");}
+function linkqq188() {location.replace("https://qq188jwh.com/");}
+function jumpqq188() {location.replace("https://qq188jwh.com/");}
+function linkqq101() {location.replace("https://qq101ldw.com/");}
+function jumpqq101() {location.replace("https://qq101ldw.com/");}
+function linkqqbonus() {location.replace("https://www.qqbonusmat.com/");}
+function jumpqqbonus() {location.replace("https://www.qqbonusmat.com/");}
+function linkqqangpao() {location.replace("https://qqangpaosnake.comMember/StreamlinedRegister?R=38B9DA8D");}
+function jumpqqangpao() {location.replace("https://qqangpaosnake.com/Member/StreamlinedRegister?R=38B9DA8D");}
+function link1001win() {location.replace("https://1001winview.com/");}
+function jump1001win() {location.replace("https://1001winview.com/");}
+function link88idr() {location.replace("https://88idrbahu.com/");}
+function jump88idr() {location.replace("https://88idrbahu.com/");}
+function linkqqresmi() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function jumpqqresmi() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkqqpusat() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function jumpqqpusat() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkqqgacor() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function jumpqqgacor() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkqq828() {location.replace("https://qq828kr1.com/");}
+function jumpqq828() {location.replace("https://qq828kr1.com/");}
+function linkqq801() {location.replace("https://qq801b3o.com/");}
+function jumpqq801() {location.replace("https://qq801b3o.com/");}
+function linkqq808() {location.replace("https://qq808scg.com/");}
+function jumpqq808() {location.replace("https://qq808scg.com/");}
+function linkqq882() {location.replace("https://qq882bfp.com/");}
+function jumpqq882() {location.replace("https://qq882bfp.com/");}
+function linkqqberak() {location.replace("https://qqberak57y.com/");}
+function jumpqqberak() {location.replace("https://qqberak57y.com/");}
+function linkqq88pro() {location.replace("https://qq88pro89r.com/");}
+function jumpqq88pro() {location.replace("https://qq88pro89r.com/");}
+function linkqq303() {location.replace("https://qq303zt4.com/Member/StreamlinedRegister?R=5FC84B59");}
+function jumpqq303() {location.replace("https://qq303zt4.com/Member/StreamlinedRegister?R=5FC84B59");}
+function linkqqfortuna() {location.replace("https://qqfortuna8zp.com/");}
+function jumpqqfortuna() {location.replace("https://qqfortuna8zp.com/");}
+function linkqqbetwin() {location.replace("https://qqbetwin8t3.com/");}
+function jumpqqbetwin() {location.replace("https://qqbetwin8t3.com/");}
+function linkqqfunbet() {location.replace("https://qqfunbet86v.com/");}
+function jumpqqfunbet() {location.replace("https://qqfunbet86v.com/");}
+function linkqqvegas() {location.replace("https://qqvegas1ak.com/");}
+function jumpqqvegas() {location.replace("https://qqvegas1ak.com/");}
+function linkqqkingbet() {location.replace("https://qqkingbet95b.com/");}
+function linkqqextra() {location.replace("https://qqextrausb.com/");}
+function linkqqslebew() {location.replace("https://qqslebewc76.com/");}
+function linkqqterang() {location.replace("https://www.qqbiru147.com/");}
+function linkqqhobi() {location.replace("https://qqhobi41t.com/");}
+function linkqq7887() {location.replace("https://qq7887bcp.com/");}
+function linkqqfunwin() {location.replace("https://qqfunwin8vk.com/");}
+function linkqqmegah() {location.replace("https://qqmegah87w.com/");}
+function linkbebek368() {location.replace("https://bebek368c.com/");}
+function linkqqfun77() {location.replace("https://qqfun77pg1.com/");}
+function linkqq999bet() {location.replace("https://qq999bet5hp.com/");}
+function linkqq7997() {location.replace("https://qq7997tg3.com/");}
+function linkqqpusat() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkqqcasino() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkdewa86() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkqqslot() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function jumpqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkqq8889() {location.replace("https://qq8889vgv.com/");}
+function linkqq1x2() {location.replace("https://qq1x2fs9.com/");}
+
+
+
+
+
+
+
