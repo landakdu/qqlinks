f0ad0b0289dec558486cff5f3cc54517b6333019
judisuhu1-ux <judisuhu1@gmail.com>
2026-01-27 03:06:29 +0800

    Update qqlink.js

--- qqlink.js
+++ qqlink.js
@@ -1,6 +1,7 @@
 document.getElementById('yourform').submit();
 function jumpnama() {window.open("/");}
 function linknama() {location.replace("/");}
+function linkqqemas() {location.replace("https://secure.livechatinc.com/customer/action/open_chat?license_id=9689690&group=1");}
 function linkqqnusa() {location.replace("https://secure.livechatinc.com/customer/action/open_chat?license_id=12597978&group=0");}
 function linkqqmacan() {location.replace("https://secure.livechatinc.com/customer/action/open_chat?license_id=16511436&group=0");}
 function linkqq101() {location.replace("https://secure.livechatinc.com/customer/action/open_chat?license_id=13374150&group=1");}
@@ -82,6 +83,7 @@ function linkcmd365() {location.replace("https://www.senisastra.com/Member/Strea
 function linksport() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function linktutup() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function link() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+
 
 
 
