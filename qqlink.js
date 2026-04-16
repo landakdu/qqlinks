f21ade5af06cc096771711b0599b55bb2250b70e
judisuhu1-ux <judisuhu1@gmail.com>
2026-04-17 06:04:44 +0800

    Update qqlink.js

--- qqlink.js
+++ qqlink.js
@@ -57,8 +57,9 @@ function linkqqbetwin() {location.replace("https://qqbetwin69q.com/");}
 function linkqqbetwinx() {location.replace("https://qqlink.pages.dev/referral-2/");}
 function linkqqfunbet() {location.replace("https://qqfunbet12p.com/");}
 function linkqqfunbetx() {location.replace("https://qqlink.pages.dev/referral-1/");}
-function linkqqvegasx() {location.replace("https://qqvegas2bt.com/Member/StreamlinedRegister?R=D1DF09BC");}
-function linkqqvegas() {location.replace("https://qqlink.pages.dev/referral-2/");}
+function linkqqvegasref() {location.replace("https://qqvegas2bt.com/Member/StreamlinedRegister?R=D1DF09BC");}
+function linkqqvegas() {location.replace("https://qqvegas2bt.com/");}
+function linkqqvegasx() {location.replace("https://qqlink.pages.dev/referral-2/");}
 function linkqqkingbet() {location.replace("https://www.qqkingbet24.com/");}
 function linkqqkingbetx() {location.replace("https://qqlink.pages.dev/referral-2/");}
 function linkqqextra() {location.replace("https://qqextra21k.com/");}
