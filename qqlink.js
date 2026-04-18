fb9bf5cbe96da23a6e3e8ed9874f27ad15dadafd
judisuhu1-ux <judisuhu1@gmail.com>
2026-04-18 23:36:23 +0800

    Update qqlink.js

--- qqlink.js
+++ qqlink.js
@@ -1,3 +1,4 @@
+function linkqq88asia() {location.replace("https://enjoythegame.online/qq88asia");}
 function linkqq8821() {location.replace("https://enjoythegame.online/qq8821");}
 function linkqqkuda() {location.replace("https://cutt.ly/Daftar-QQKuda");}
 function linkgoldwin678() {location.replace("https://masuk.gold/");}
