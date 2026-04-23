8eab6d517d5e57afc0ddc65fdb88d3646ec47a33
judisuhu1-ux <judisuhu1@gmail.com>
2026-04-23 13:57:31 +0800

    Update qqlink.js

--- qqlink.js
+++ qqlink.js
@@ -84,7 +84,8 @@ function linkqq999betx() {location.replace("https://qqlink.pages.dev/referral-1/
 function linkqq7997() {location.replace("https://qq7997bs2.com/");}
 function linkqq7997x() {location.replace("https://qqlink.pages.dev/referral-2/");}
 function linkqqbos99() {location.replace("https://qqlink.pages.dev/referral-2/");}
-function linkqqbos99x() {location.replace("https://senisastra.com/");}
+function linkqqbos99x() {location.replace("https://qqbos99p1.com/");}
+function linktutup() {location.replace("https://amplob-ri.pages.dev/link/");}
 function linkqq8889() {location.replace("https://rebrand.ly/loginqq8889");}
 function linkqq8889x() {location.replace("https://qqlink.pages.dev/referral-1/");}
 function linkqq1x2() {location.replace("https://lkgw.net/qq1x2-altbrand");}
